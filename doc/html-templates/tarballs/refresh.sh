1d6b60467aa43c15544a36fba6f249fa08861e8f
tripu <t@tripu.info>
2018-08-03 13:21:16 +0200

    HTML templates: rework structure and index

--- doc/html-templates/tarballs/refresh.sh
+++ doc/html-templates/tarballs/refresh.sh
@@ -9,7 +9,7 @@ rm *.tar.gz
 declare -A templates=( [4]=separate-css-js [5]=metadata [6]=libraries [7]=responsive )
 
 for temp in "${!templates[@]}"; do
-    cp -r ../$temp-${templates[$temp]}/ canon-template-${templates[$temp]}
-    tar -czf canon-template-${templates[$temp]}.tar.gz canon-template-${templates[$temp]}/
-    rm -rf canon-template-${templates[$temp]}/
+    cp -r ../$temp-${templates[$temp]}/ canon-html-template-${templates[$temp]}
+    tar -czf canon-html-template-${templates[$temp]}.tar.gz canon-html-template-${templates[$temp]}/
+    rm -rf canon-html-template-${templates[$temp]}/
 done
